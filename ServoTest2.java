28f206421eecf6c84a897493755eac5a9bb7554c
Addy Dixon <piggieo@dixonclan.org>
2024-02-10 15:01:11 -0600

    Program for a servo to move with the left stick y-axis

---
+++ ServoTest2.java
@@ -0,0 +1,33 @@
+package Centerstage;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name = "ServoTest2") 
+public class ServoTest2 extends LinearOpMode {
+
+    private CRServo a;
+    
+    @Override
+    public void runOpMode() {
+        a = hardwareMap.get(CRServo.class, "servo");
+      
+        a.setDirection(DcMotor.Direction.FORWARD);
+      
+        waitForStart();
+
+        if (opModeIsActive()) {
+            a.setPower(0);
+
+            while(opModeIsActive()) {
+                telemetry.addData("Left stick up/down position", gamepad1.left_stick_y);
+                telemetry.update();
+                a.setPower(gamepad1.left_stick_y);
+            }
+        }
+    }
+}    
